3b25834cb4c0828e5dbbcac121056b9a0f34317c
paulwooolf <paulwooolf@gmail.com>
2021-04-03 22:13:24 +0300

    file loader configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,15 @@ module.exports = {
         rules: [
             {
                 test: /\.(svg|png)$/,
-                use: [{loader: 'file-loader'}]
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                            name: '[name]-[sha1:hash:7].[ext]'
+                        }
+                    }
+                    ]
             }
         ]
     }
