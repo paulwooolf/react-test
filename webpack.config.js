f4f220e739e81c65441b325fc383c72899f80d67
paulwooolf <paulwooolf@gmail.com>
2021-04-04 15:33:21 +0300

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: "development",
     module: {
@@ -48,5 +50,10 @@ module.exports = {
                 ],
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ]
 };
